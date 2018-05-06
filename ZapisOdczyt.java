5d2bcc7e276001ecaac4d8d4f517812056342d17
pdzanek <justfushi@gmail.com>
2018-05-06 13:00:11 +0200

    added java.io

--- ZapisOdczyt.java
+++ ZapisOdczyt.java
@@ -4,13 +4,46 @@ import java.nio.*;
 
 public class ZapisOdczyt{
 	public static void main(String[] args){
-	 Random randChar = new Random();
+	long start, koniec;
+	Random randChar = new Random();
 	char charTable[] = new char[1000];
-	char charBrzezio[] = new char[1000];
 	for(int i=0;i<1000;i++){
 	 	charTable[i]=(char)(randChar.nextInt(94)+33);
 		System.out.print(charTable[i]);
 	}
+	try{
+ 		start=System.currentTimeMillis();
+		File fileIO= new File("zapisIO.txt");
+		PrintWriter zapisIO = new PrintWriter(fileIO);
+			zapisIO.println(charTable);
+			zapisIO.close();
+		char odczytaneIO[] = new char[1000];
+		FileReader odczytIO = new FileReader(fileIO);
+			odczytIO.read(odczytaneIO);
+			odczytIO.close();
+		koniec=System.currentTimeMillis();
+		System.out.println(odczytaneIO);
+		System.out.println("java.io czas: "+(koniec-start));
+		
+	}catch(Exception e)
+		{
+			System.out.println(e.getMessage());	
+		}
+
+		try{
+		start=System.currentTimeMillis();
+		FileChannel fileChannel = new FileOutputStream("zapisNIO.txt").getChannel();
+
+		char[] pobrane=new char[1000];
+		for(int i=0;i<1000;i++) odczytaneNIO[i]=(char)b[i];
+		koniec=System.currentTimeMillis();
+		System.out.println(odczytaneNIO);
+		System.out.println("java.nio czas: "+(koniec-start));
+		
+	}catch(Exception e)
+		{
+			System.out.println(e.getMessage());	
+		}
 	}
 }
 
